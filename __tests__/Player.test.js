9c65cbd7fae7c1842f27b5049cb1eebdfced8d2b
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-09-15 02:45:55 -0500

    made a player object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,13 +1,20 @@
-const { exportAllDeclaration } = require('@babel/types');
-const { default: test } = require('node:test');
-const Player = require('../lib/Player');
 
-test('creates a player object', () => {
-    const player = new Player('Dave');
+const Potion = require('../lib/Potion.js');
 
-    expect(player.name).toBe('Dave');
-    expect(player.health).toEqual(expect.any(NUmber));
-    expect(player.strength).toEqual(expect.any(Number));
-    ecpect(player.agility).toEqual(expect.any(Number));
+test('creates a health potion object', () => {
+    const potion = new Potion('health');
+  
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
+  
 
+test('creates a health potion object', () => {
+      
+      const potion = new Potion();
+  
+      expect(potion.name).toEqual(expect.any(String));
+      expect(potion.name.length).toBeGreaterThan(0);
+      expect(potion.value).toEqual(expect.any(Number));
+  
 });
